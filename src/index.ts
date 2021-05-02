30bcd5f3d0e964791fc9eb04c8d58233e9ce07c7
s3822042 <s3822042@rmit.edu.vn>
2021-05-03 01:17:30 +0700

    fix package module conflict

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,11 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
-import redis from 'redis';
-import session from 'express-session';
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
-let RedisStore = require('connect-redis')(session)
-let redisClient = redis.createClient()
 
-app.use(
-  session({
-    store: new RedisStore({ client: redisClient }),
-    saveUninitialized: false,
-    secret: 'keyboard cat',
-    resave: false,
-  })
-)
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -30,12 +21,35 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__ // cookies only work on https
+
+      },
+      saveUninitialized: false,
+      secret: 'dasdasfafadasdqwwasd',
+      resave: false,
+    })
+)
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({req, res}) => ({ em: orm.em , req , res }),
   });
 
   apolloServer.applyMiddleware({ app });
