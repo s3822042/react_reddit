7a50490f43037db6c88a691d74c13fbfedf29568
s3822042 <s3822042@rmit.edu.vn>
2021-05-02 22:56:28 +0700

    add register function

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,35 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
-    
-    const app = express();
-    
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver , PostResolver],
-            validate: false
-        }),
-        context: () => ({ em: orm.em })
-    })
+  const app = express();
 
-    apolloServer.applyMiddleware({app})
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
 
-    app.listen(4000, () => {
-        console.log("Server started on port 4000")
-    })
+  apolloServer.applyMiddleware({ app });
 
-}
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
 main().catch((err) => {
-    console.log(err);
+  console.error(err);
 });
