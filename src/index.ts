710378897d13e59bf6421bf815c12881b1aae290
s3822042 <s3822042@rmit.edu.vn>
2021-05-02 21:02:57 +0700

    Apollo Server Express Setup and MikroORM TypeGraphQL Crud

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
 
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, { title: 'my first post' });
-    // await orm.em.persistAndFlush(post);
+    
+    const app = express();
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver , PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts)
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => {
+        console.log("Server started on port 4000")
+    })
 
 }
 main().catch((err) => {
